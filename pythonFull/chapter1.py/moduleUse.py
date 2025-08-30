433fe5e29e4bad0dbba41a4ba15ec59ba4416920
Rahul Ahirwar <rahulahirwar4089@gmail.com>
2025-08-30 13:32:03 +0530

    ok

---
+++ pythonFull/chapter1.py/moduleUse.py
@@ -0,0 +1,10 @@
+
+# a module is a file they 
+# containing code written by 
+# somebody else which can imported 
+# another program . like pip install pandas 
+
+import pyjokes
+
+jokes = pyjokes.get_joke()
+print("jokes :" +jokes)
