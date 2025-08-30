9185f5bd649dd823d6b38c94593c1b612d6447d4
Rahul Ahirwar <rahulahirwar4089@gmail.com>
2025-08-30 15:32:37 +0530

    ok

---
+++ pythonFull/chapter4/01_list.py
@@ -0,0 +1,16 @@
+freinds = ["apple",5,20.332,"mangoes","rohan",False]
+freinds[0] = "hello"
+
+# print(freinds)
+# print(freinds[1:5])
+
+#string are immutable but list is mutuble ;
+
+# freinds.append("rahul")
+# print(freinds)
+
+l1 = [6,3,22,424,22,1,2,4]
+# l1.sort()
+# print(l1)
+l1.insert(2,112)
+print(l1)
