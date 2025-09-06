0e059ba9ce0af910a8d3349da49c689614bbc9cf
Rahul Ahirwar <rahulahirwar4089@gmail.com>
2025-09-06 10:32:09 +0530

    ok

---
+++ ModulesPython/numpyModule/MultipleArray_07.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+a = np.random.random(1)
+print(a)
+b = np.random.random((2,2))
+print(b)
+# c = np.random.randint(1,10)
+# c = np.random.randint(1,10,(2,2))
+# c = np.random.randint(1,10,(3,4,5))
+# c = np.random.rand(2,2)
+# c = np.random.randn(2,2)
+
+c = np.arange(1,10)
+d=np.random.choice(c)
+
+print(d)
---
+++ ModulesPython/numpyModule/MultipleArray_08.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+s1 = " tony stark was able to" \
+" build this in a cave with " \
+"scurb of boxes "
+s2 = " sorry sir but i am not tony stark"
+
+a = np.char.add(s1,s2)
+"""
+upper
+lower
+split
+splitlines
+replace
+center
+
+"""
+print(a)
